83094c7f4bf7b4d5e84afd0ea9c4312e70ee2fe6
nsb329 <nsb329@naver.com>
2024-11-06 11:49:18 +0900

    VO를 통해 데이터 관리

---
+++ src/main/java/store/FileinVO.java
@@ -0,0 +1,32 @@
+package store;
+
+public class FileinVO {
+	private String name;
+	private int price;
+	private int quantity;
+	private String promotion;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public String getPromotion() {
+		return promotion;
+	}
+	public void setPromotion(String promotion) {
+		this.promotion = promotion;
+	}
+}
